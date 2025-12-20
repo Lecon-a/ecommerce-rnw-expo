76802b85a734ad16eac8d3dd4170d19f89ced8c6
Sunday P. Afolabi <spog4true@gmail.com>
2025-12-19 20:47:35 -0800

    edit env.js by optional values

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export const ENV = {
-    MODE_ENV: process.env.MODE_ENV,
-    PORT: process.env.PORT,
+    MODE_ENV: process.env.MODE_ENV || "development",
+    PORT: process.env.PORT || 3000,
     DB_URL: process.env.DB_URL,
-}
+} 
