3d5357910540437ced485ec4bad8e5c68cc6d559
Sunday P. Afolabi <spog4true@gmail.com>
2025-12-22 11:27:12 -0800

    implement user's routes

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+    addAddress,
+    updateAddress,
+    getAddresses,
+    deleteAddress,
+    addToWishlist,
+    getWishlist,
+    removeFromWishlist
+} from "../controllers/user.controller.js";
+import { protecteRoute } from "../middleware/auth.middleware.js";
+ 
+const userRouter = Router();
+userRouter.use(protecteRoute)
+// address route 
+userRouter.post("/addresses", addAddress)
+userRouter.get("/addresses", getAddresses)
+userRouter.put("/addresses/:addressId", updateAddress)
+userRouter.delete("/addresses/:addressId", deleteAddress)
+
+// wishlist route
+userRouter.post("/wishlist", addToWishlist)
+userRouter.get("/wishlist", getWishlist)
+userRouter.delete("/wishlist/:productId", removeFromWishlist)
+
+export default userRouter;
